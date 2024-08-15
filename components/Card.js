f6c5ee9097ceb7f60225ba270ef2623959fd5a46
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-14 21:04:46 -0600

    new style for the card

--- components/Card.js
+++ components/Card.js
@@ -4,6 +4,7 @@ import React from 'react'
 export default function Card({ item }) {
     return (
         <View style={styles.container}>
+            <Text style={styles.textCard}>{item.id}</Text>
             <Text style={styles.textCard}>{item.title}</Text>
             <Text style={styles.textBody}>{item.body}</Text>
         </View>
@@ -13,20 +14,23 @@ export default function Card({ item }) {
 
 const styles = StyleSheet.create({
     container: {
-        width: "100",
-        height: 250,
+        width: "100%",
+        height: "auto",
         backgroundColor: '#D5ED9F',
         borderRadius: 16,
-        padding: 10
+        paddingTop: 12,
+        paddingBottom: 20,
+        paddingLeft: 10,
+        paddingRight: 10
     },
     textCard: {
         textAlign: "center",
-        fontSize: 20,
+        fontSize: 30,
         fontWeight: "bold",
         marginBottom: 20
     },
     textBody: {
         textAlign: "center",
-        fontSize: 16,
+        fontSize: 20,
     }
 });
